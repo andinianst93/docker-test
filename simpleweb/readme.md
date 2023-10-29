eee077d003ab3df7b68dbeaf013d4458271e8e5d
andinianst93 <andinianst93@gmail.com>
2023-10-30 03:40:14 +0700

    simpleweb added

---
+++ simpleweb/readme.md
@@ -0,0 +1,8 @@
+# Docker
+
+### Buildkit and using the progress flag:
+
+```docker build --progress=plain ```
+
+```docker build --no-cache --progress=plain```
+
---
+++ simpleweb/src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (_req: Request, res: Response) => {
+    res.render('index');
+});
+
+
+// PORT
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
